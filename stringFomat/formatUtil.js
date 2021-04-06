c7980ec851012fae1e998ac8f86d3e7ad477945a
wooklee <zckxw@naver.com>
2021-04-06 15:22:18 +0900

    stringFormatingRegExpv1.0

--- stringFomat/formatUtil.js
+++ stringFomat/formatUtil.js
@@ -1,36 +1,34 @@
 
 function formatStringRegExp(value, format, event){
-    //구분자를 뺀 숫자
-    var valueTmp = value.replace(/[^a-zA-Z0-9]/gi,'');
-    //{...}들 
-    var formatValArray = format.split(/[^\{\w+\}]/).filter(e=>e);
-    console.log(formatValArray);
-    var formatValLen = 0;
-    var formatTmp = format;
 
     //backSpace입력시 value값 return;
     if(event.originalEvent.inputType === "deleteContentBackward"){
-        let formatTmp = format.replace(/[\{\}]/g,'' );
-        for(let i = 0 ; i < formatValArray.length ; i++){
-            
-        }
-        console.log(formatTmp);
         return value;
     }
-    
-        
+
+    //구분자를 뺀 숫자
+    var resultValue = value.replace(/[^a-zA-Z0-9]/gi,'');
+    //{...}들 
+    var formatValArray = format.split(/[^\{\w+\}]/).filter(e=>e);
+    var valTotalLen = 0;
+    var captureFormat = format;
+
+    console.log(formatValArray);
+
     for(var i = 0 ; i < formatValArray.length ; i++){
 
+        let valLen = formatValArray[i].length-2; //format의 {0000}안의 value개수 
         var captureNum = '$'+(i+1);
-        formatTmp = formatTmp.replace(formatValArray[i], captureNum);
-        formatValLen += formatValArray[i].length-2;
-        formatValArray[i] = '(\\w{0,'+ String(formatValArray[i].length-2) +'})';
 
-        if(formatValLen > valueTmp.length){
-            if(i === formatValArray.length-1 && formatValLen === valueTmp.length){
-                formatTmp = formatTmp.substring(0,  formatTmp.length);
+        captureFormat = captureFormat.replace(formatValArray[i], captureNum); //1$ - 2$ ..... 형식으로 변형
+        valTotalLen += valLen; //format기준으로 value 최대길이
+        formatValArray[i] = '(\\w{0,'+ String(valLen) +'})'; //정규식 생성.
+
+        if(valTotalLen > resultValue.length){
+            if(i === formatValArray.length-1 && valTotalLen === resultValue.length){
+                captureFormat = captureFormat.substring(0,  captureFormat.length);
             }else{
-                formatTmp = formatTmp.substring(0,  formatTmp.indexOf(captureNum)+2);
+                captureFormat = captureFormat.substring(0,  captureFormat.indexOf(captureNum)+2);
             }
             formatValArray = formatValArray.slice(0, i+1);
             break;
@@ -39,62 +37,61 @@ function formatStringRegExp(value, format, event){
     var regExp = new RegExp(formatValArray.join(''))  
 
     console.log(regExp);
-    valueTmp = valueTmp.replace(regExp,formatTmp);
+    resultValue = resultValue.replace(regExp, captureFormat); 
 
-    //length 초과 시 
-    if(valueTmp.length === format.length-(formatValArray.length*2)+1){
-        return valueTmp.slice(0, valueTmp.length-1);
+     //length 초과 시 
+     if(resultValue.length === format.length-(formatValArray.length*2)+1){
+        resultValue = resultValue.slice(0, resultValue.length-1)
     }
-    
-    return valueTmp;
+
+    let position = resultValue.length; //default position
+    let prevPosition = event.target.selectionStart; //textInput 적용전 position
+
+    //textInput에 적용, 적용하면 자동적으로 커서가 맨 뒤로 위치하게 됨.
+    $(event.target).val(resultValue);
+
+    //set cursor position , 중간위치에서 수정 시 커서가 그 자리에 있도록.
+    if(event.originalEvent.inputType !== "deleteContentBackward"){
+        if(resultValue.length > prevPosition && resultValue.substring(prevPosition+1, resultValue.length).replace(/[^0-9]/g,'')){
+            position = prevPosition;
+        }       
+        event.target.setSelectionRange(position,position);
+    }
 }
-
-$('#dateTextInput').on('input', (event)=>{
-    //console.log(event.target.value);
-    $(event.target).val(formatString(event.target.value, "1234-56-78"));
-});
-
-$('#timeTextInput').on('input', (event)=>{
-    //console.log(event.target.value);
-    
-    $(event.target).val(formatStringRegExp(event.target.value, '{00}:{00}:{00}', event));
-});
 
 $('#formatTextInput').on('input', (event)=>{
     console.log(event.target.selectionStart);
-    $(event.target).val(formatStringRegExp(event.target.value, '{0000}-{00}-{00}', event));
-    event.target.selectionStart = event.target.event-1;
-    
+    formatStringRegExp(event.target.value, '{0000}-{00}-{00}', event);
 });
 
 $('#format2TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '{0000}년{00}월{00}일', event));
+    formatStringRegExp(event.target.value, '{0000}년{00}월{00}일', event);
 });
 
 $('#format3TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '{00}-{0}-{0000}', event));
+    formatStringRegExp(event.target.value, '{00}-{0}-{0000}', event);
 }); 
 
 $('#format4TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '[{00}]-[{00}]-[{00}]', event));
+    formatStringRegExp(event.target.value, '[{00}]-[{00}]-[{00}]', event);
 });
 
 $('#format5TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '{0000}년{00}월{00}일 {00}시{00}분{00}초', event));
+    formatStringRegExp(event.target.value, '{0000}년{00}월{00}일 {00}시{00}분{00}초', event);
 });
 
 $('#format6TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '{0000}년{00}월{00}일 {00}:{00}:{00}', event));
+    formatStringRegExp(event.target.value, '{0000}년{00}월{00}일 {00}:{00}:{00}', event);
 });
 
 $('#format7TextInput').on('input', (event)=>{
     //console.log(event);
-    $(event.target).val(formatStringRegExp(event.target.value, '({000})-{0000}-{0000}({ssss})', event));
+    formatStringRegExp(event.target.value, '({000})-{0000}-{0000}({ssss})', event);
 });
 
 
